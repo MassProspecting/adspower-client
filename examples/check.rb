3365b47c77ba8de461a01699a2d213c8c0129ac9
leandrosardi <leandro.sardi@expandedventure.com>
2024-02-07 15:29:45 -0300

    #4

---
+++ examples/check.rb
@@ -0,0 +1,24 @@
+# Avoid memory leaks.
+#
+# Reference: https://github.com/leandrosardi/adspower-client/issues/4
+# 
+
+require 'adspower-client'
+#require_relative '../lib/adspower-client'
+require 'pry'
+
+l = BlackStack::LocalLogger.new('check.log')
+
+ADSPOWER_API_KEY = '00db0bb239d8c95acbfdf03ab8eb1414'
+PROFILE_ID = 'jdus77h'
+
+client = AdsPowerClient.new(key: ADSPOWER_API_KEY)
+
+# open the browser
+driver = client.driver(PROFILE_ID)
+
+# show the number of `chromedriver` processes running
+puts `ps aux | grep "chromedriver"`
+
+# breakpoint for you, to experiment starting and closseing drivers
+binding.pry
